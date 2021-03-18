5a235d114c708864d69467f67be264e02ae3d48a
Nemanja Porobic <n.porobic@vega.local>
2021-03-18 13:08:01 +0100

    update project

--- vue-frontend/tests/unit/components/Footer.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import Footer from '@/components/Footer.vue'
-
-describe('Footer.vue', () => {
-    test('footer snapshot', () => {
-      const wrapper = shallowMount(Footer)
-      expect(wrapper).toMatchSnapshot()
-    })
-  })
